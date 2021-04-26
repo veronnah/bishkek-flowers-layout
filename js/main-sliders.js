0abb8cfa2e49dde2dcdb34e55db482751798ef17
veronnah <yoo.veroon@gmail.com>
2021-04-26 12:15:05 +0200

    slider with articles added

--- js/main-sliders.js
+++ js/main-sliders.js
@@ -1,20 +1,49 @@
-const homeSlider = new Swiper('.swiper-container', {
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper__btn-next',
-                prevEl: '.swiper__btn-prev',
-                disabledClass: 'swiper__btn-disable',
-            },
-            loop: false,
-            autoplay: true,
-            responsive: true,
-            speed: 800,
-            effect: 'slide',
-            fadeEffect: {
-                crossFade: true
-            },
-        
+let homeSlider = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper__btn-next',
+        prevEl: '.swiper__btn-prev',
+        disabledClass: 'swiper__btn-disable',
+    },
+    loop: false,
+    autoplay: true,
+    responsive: true,
+    speed: 800,
+    effect: 'slide',
+    fadeEffect: {
+        crossFade: true
+    },
+});
+var swiper = new Swiper('.articles__swiper-container', {
+    slidesPerView: 4.5,
+    spaceBetween: 20,
+    loop: false,
+    autoplay: true,
+    responsive: true,
+    breakpoints: {
+        290:{
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+        320: {
+          slidesPerView: 1.5,
+          spaceBetween: 20
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        1000:{
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+           
+        }
+      }
 });
