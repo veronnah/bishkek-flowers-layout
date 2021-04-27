4ed5354c05b04a8af13a1bf2ec7b53f073e82612
veronnah <yoo.veroon@gmail.com>
2021-04-27 14:28:31 +0200

    buy one click modal added

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ let toggleMenu = function () {
   searchGroup.classList.toggle('active');
   body.classList.toggle('disable');
   closeBtn.classList.toggle('active');
-  console.log(closeBtn);
 }
 
 searchBtn.addEventListener('click', function (e) {
@@ -74,3 +73,73 @@ const config = {once: true};
 Array.from(player).forEach(function(player){
   player.addEventListener('click', loadPlayer, config);
 })
+
+
+// cart menu 
+const cartBtn = document.querySelector('.cart-btn');
+const cartMenu = document.querySelector('.cart-menu');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const cartToggle = function() {
+  cartMenu.classList.toggle('open');
+  overlay.classList.toggle('open');
+  body.classList.toggle('disable');
+}
+
+cartBtn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    cartToggle();
+
+});
+
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == cartMenu || cartMenu.contains(target);
+    const its_btnMenu = target == cartBtn;
+    const menu_is_active = cartMenu.classList.contains('open');
+    
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      cartToggle();
+    }
+});
+
+function closeModal() {
+  cartMenu.classList.remove('open');
+  overlay.classList.remove('open');
+  body.classList.remove('disable');
+}
+
+//buy one click
+let buyOneModal = document.querySelector('.buy__one_click-menu');
+let buyBtn = document.getElementsByClassName('buy-btn');
+let closeBuyBtn = document.getElementsByClassName('close-buy__one')[0];
+let buyOverlay = document.querySelector('.buy-overlay');
+
+
+for (var i = 0; i < buyBtn.length; i++) {
+  buyBtn[i].addEventListener('click', function() {
+    openModalWindow();
+  })
+}
+
+closeBuyBtn.addEventListener('click', function() {
+  closeModalWindow();
+})
+
+function openModalWindow() {
+  buyOneModal.classList.toggle('open');
+  buyOverlay.classList.toggle('open');
+  body.classList.toggle('disable');
+}
+
+function closeModalWindow() {
+  buyOneModal.classList.remove('open');
+  buyOverlay.classList.remove('open');
+  body.classList.remove('disable');
+}
+
+window.onclick = function(event) {
+  if (event.target == buyOverlay) {
+    closeModalWindow();
+  }
+}
