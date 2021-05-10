e59de4668907d965ed2784ff7ab6e96e2f8db02c
veronnah <yoo.veroon@gmail.com>
2021-05-10 13:29:47 +0200

    catalog page: filter menu

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let searchBtn = document.querySelector('.header__nav-search');
 let searchGroup = document.querySelector('.header-bottom__search-group');
 let searchInput = document.querySelector('.header-bottom__search');
 let closeBtn = document.querySelector('.close-search');
-let body = document.querySelector('.body');
+var body = document.querySelector('.body');
 let toggleMenu = function () {
   searchInput.classList.toggle('active');
   searchGroup.classList.toggle('active');
@@ -57,20 +57,21 @@ function closeMenu() {
 const player = document.querySelectorAll('.video__section-player');
 
 const loadPlayer = (event) => {
-  const target = event.currentTarget, 
-  iframe = document.createElement('iframe');
+  const target = event.currentTarget,
+    iframe = document.createElement('iframe');
   iframe.src = 'https://www.youtube.com/embed/' + target.dataset.videoId + '?&autoplay=1&mute=1';
   iframe.setAttribute('frameborder', 0);
   target.classList.remove('btn-dis');
-  if(target.children.length){
+  if (target.children.length) {
     target.replaceChild(iframe, target.firstElementChild);
-  }
-  else{
+  } else {
     target.appendChild(iframe)
   }
 }
-const config = {once: true};
-Array.from(player).forEach(function(player){
+const config = {
+  once: true
+};
+Array.from(player).forEach(function (player) {
   player.addEventListener('click', loadPlayer, config);
 })
 
@@ -78,29 +79,29 @@ Array.from(player).forEach(function(player){
 // cart menu 
 const cartBtn = document.querySelector('.cart-btn');
 const cartMenu = document.querySelector('.cart-menu');
-const overlay = document.querySelector('.overlay');
+var overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
-const cartToggle = function() {
+const cartToggle = function () {
   cartMenu.classList.toggle('open');
   overlay.classList.toggle('open');
   body.classList.toggle('disable');
 }
 
-cartBtn.addEventListener('click', function(e) {
-    e.stopPropagation();
-    cartToggle();
+cartBtn.addEventListener('click', function (e) {
+  e.stopPropagation();
+  cartToggle();
 
 });
 
-document.addEventListener('click', function(e) {
-    const target = e.target;
-    const its_menu = target == cartMenu || cartMenu.contains(target);
-    const its_btnMenu = target == cartBtn;
-    const menu_is_active = cartMenu.classList.contains('open');
-    
-    if (!its_menu && !its_btnMenu && menu_is_active) {
-      cartToggle();
-    }
+document.addEventListener('click', function (e) {
+  const target = e.target;
+  const its_menu = target == cartMenu || cartMenu.contains(target);
+  const its_btnMenu = target == cartBtn;
+  const menu_is_active = cartMenu.classList.contains('open');
+
+  if (!its_menu && !its_btnMenu && menu_is_active) {
+    cartToggle();
+  }
 });
 
 function closeModal() {
@@ -117,12 +118,12 @@ let buyOverlay = document.querySelector('.buy-overlay');
 
 
 for (var i = 0; i < buyBtn.length; i++) {
-  buyBtn[i].addEventListener('click', function() {
+  buyBtn[i].addEventListener('click', function () {
     openModalWindow();
   })
 }
 
-closeBuyBtn.addEventListener('click', function() {
+closeBuyBtn.addEventListener('click', function () {
   closeModalWindow();
 })
 
@@ -138,7 +139,7 @@ function closeModalWindow() {
   body.classList.remove('disable');
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == buyOverlay) {
     closeModalWindow();
   }
@@ -147,22 +148,27 @@ window.onclick = function(event) {
 // input number 
 var spins = document.getElementsByClassName("spin");
 for (var i = 0, len = spins.length; i < len; i++) {
-    var spin = spins[i],
-        span = spin.getElementsByTagName("span"),
-        input = spin.getElementsByTagName("input")[0];
-    
-    input.onchange = function() { input.value = +input.value || 0; };
-    span[0].onclick = function() { input.value = Math.max(0, input.value - 1); };
-    span[1].onclick = function() { input.value -= -1; };
+  var spin = spins[i],
+    span = spin.getElementsByTagName("span"),
+    input = spin.getElementsByTagName("input")[0];
+
+  input.onchange = function () {
+    input.value = +input.value || 0;
+  };
+  span[0].onclick = function () {
+    input.value = Math.max(0, input.value - 1);
+  };
+  span[1].onclick = function () {
+    input.value -= -1;
+  };
 }
 
 // accordeon
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -171,7 +177,7 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
       panel.style.paddingBottom = '10px';
-    } 
+    }
   });
 }
 
@@ -180,27 +186,26 @@ let priceRanges = document.querySelectorAll('.js-price-range');
 
 priceRanges.forEach(el => {
   let downPriceInput = el.closest('.filter-price').querySelector('.js-price-down'),
-  upPriceInput = el.closest('.filter-price').querySelector('.js-price-up'),
-  inputs = [downPriceInput, upPriceInput];
+    upPriceInput = el.closest('.filter-price').querySelector('.js-price-up'),
+    inputs = [downPriceInput, upPriceInput];
 
 
-  //get maxPrice for slider price
   const maxPrice = +upPriceInput.getAttribute('data-max');
   // upPriceInput.value = maxPrice.toLocaleString() + ' p.';
 
-  //Init price range slider
   noUiSlider.create(el, {
     range: {
       'min': 0,
-      'max': maxPrice },
+      'max': maxPrice
+    },
 
     behaviour: 'drag',
     connect: true,
     start: [0, maxPrice],
-    step: 1 });
+    step: 1
+  });
 
 
-  //Update value after scroll pointer in slider
   el.noUiSlider.on('update', values => {
     let [downPrice, upPrice] = values;
 
@@ -211,7 +216,6 @@ priceRanges.forEach(el => {
     upPriceInput.value = upPrice;
   });
 
-  //Change slider value after inputs change
   inputs.forEach(function (input, handle) {
     input.addEventListener('change', function () {
       let value = this.value;
@@ -225,7 +229,6 @@ priceRanges.forEach(el => {
 });
 
 //clear sliders value
-
 let clearBtn = document.querySelector('.js-clear-sliders');
 
 clearBtn.addEventListener('click', e => {
@@ -233,7 +236,7 @@ clearBtn.addEventListener('click', e => {
 
   filterPrices.forEach(el => {
     let priceRange = el.querySelector('.js-price-range'),
-    priceRangeInputs = el.querySelectorAll('.filter-price__flex-row input');
+      priceRangeInputs = el.querySelectorAll('.filter-price__flex-row input');
 
     priceRangeInputs.forEach(function (input, handle) {
       let maxPrice = handle ? input.getAttribute('data-max') : 0;
@@ -242,3 +245,29 @@ clearBtn.addEventListener('click', e => {
     });
   });
 });
+
+
+//fiter menu
+let fiterBtn = document.querySelector('.sortby-filter__btn');
+let fiterMenu = document.querySelector('.catalog__filter');
+let filterTitle = document.querySelector('.filter-title');
+let filterOverlay = document.querySelector('.filter-overlay');
+
+function openFilter() {
+  fiterMenu.classList.toggle('active');
+  filterTitle.style.borderRadius = 'unset';
+  body.classList.toggle('disable');
+  filterOverlay.classList.toggle('open');
+}
+
+function closeFilterMenu() {
+  fiterMenu.classList.remove('active');
+  filterOverlay.classList.remove('open');
+  body.classList.remove('disable');
+}
+
+window.onclick = function (event) {
+  if (event.target == filterOverlay) {
+    closeFilterMenu();
+  }
+}
