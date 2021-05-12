fbcf79209d5216c1e6ddd089baeff41027ed515b
veronnah <yoo.veroon@gmail.com>
2021-05-12 12:02:57 +0200

    card_product page: final changes

--- js/main-sliders.js
+++ js/main-sliders.js
@@ -17,6 +17,7 @@ let homeSlider = new Swiper('.swiper-container', {
         crossFade: true
     },
 });
+
 var swiper = new Swiper('.articles__swiper-container', {
     slidesPerView: 4.5,
     spaceBetween: 20,
@@ -45,4 +46,45 @@ var swiper = new Swiper('.articles__swiper-container', {
             spaceBetween: 20,
         }
       }
+});
+
+let productsSlider = new Swiper('.products__card-swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+      nextEl: '.products-slider__btn-next',
+      prevEl: '.products-slider__btn-prev',
+      disabledClass: 'swiper__btn-disable',
+  },
+  loop: true,
+  autoplay: true,
+  responsive: true,
+  speed: 800,
+  effect: 'slide',
+  fadeEffect: {
+      crossFade: true
+  },
+  breakpoints: {
+    290:{
+        slidesPerView: 1,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1000:{
+        slidesPerView: 3,
+        spaceBetween: 20,
+    },
+    1300:{
+        slidesPerView: 4,
+        spaceBetween: 20,
+    }
+ 
+  }
 });
--- js/main.js
+++ js/main.js
@@ -271,3 +271,19 @@ window.onclick = function (event) {
     closeFilterMenu();
   }
 }
+ 
+
+// tabs
+function openTab(evt, cityName) {
+  let i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tab-bg-active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " tab-bg-active";
+}
